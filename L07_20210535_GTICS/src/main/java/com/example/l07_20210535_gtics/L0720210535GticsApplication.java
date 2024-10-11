a9f823e0c7ed754f6b6c2d60a5190a6c53f6803b
RCT4H3 <139926077+RCT4H3@users.noreply.github.com>
2024-10-10 19:16:23 -0500

    Nuevo proyecto para lab7

---
+++ L07_20210535_GTICS/src/main/java/com/example/l07_20210535_gtics/L0720210535GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.l07_20210535_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L0720210535GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L0720210535GticsApplication.class, args);
+    }
+
+}
---
+++ L07_20210535_GTICS/src/test/java/com/example/l07_20210535_gtics/L0720210535GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.l07_20210535_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L0720210535GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
